f37c7b1f679d7c8b4a69ebddb182f18019a45e33
m-signo <mirko_signorelli@hotmail.it>
2018-10-19 15:17:51 +0200

    Delete pmf.R

--- pmf.R
+++
@@ -1,9 +0,0 @@
-pmf <- function(z, xlim = NULL, title = NULL) {
-  table <- table(z)
-  x <- as.numeric(row.names(table))
-  freq <- as.numeric(table)
-  if (is.null(xlim)) xlim = c(min(x), max(x))
-  plot(NULL, xlim = xlim, ylim = c(0, max(freq)),
-       xlab = 'x', ylab = 'Frequency', main = title)
-  segments(x0 = x, x1 = x, y0 = 0, y1 = freq)
-}
